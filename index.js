b4cd39966acbc7e7b1613bed17e9add5efd8d111
istiaq-ahsan <ahsanistiaq23@gmail.com>
2024-12-05 01:03:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1pvay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const cfProjectCollection = client.db("cfProjectDB").collection("project");
+        const userCollection = client.db("cfUserDB").collection("users");
+
+        app.get('/project', async (req, res) => {
+            const cursor = cfProjectCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/project', async (req, res) => {
+            const newProjectInfo = req.body;
+            console.log(newProjectInfo);
+            const result = await cfProjectCollection.insertOne(newProjectInfo);
+            res.send(result);
+        })
+
+        app.get('/project/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cfProjectCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/project/:id', async (req, res) => {
+            const newProjectInfo = req.body;
+            console.log(newProjectInfo);
+            const result = await cfProjectCollection.insertOne(newProjectInfo);
+            res.send(result);
+        })
+
+        //user part
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await userCollection.insertOne(newUser)
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Crowdcube server');
+})
+
+app.listen(port, () => {
+    console.log(`Simple CRUD is running on port ${port}`);
+})
